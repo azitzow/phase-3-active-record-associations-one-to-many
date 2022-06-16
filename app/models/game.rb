65248526c04abb943308c1f12d4cf3686d350c9f
Ana Zitzow <anasempai89@gmail.com>
2022-06-15 18:04:06 -0700

    commit tree 027006dd378c6d09d424f58f3dd8e15a14310da3

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+
+  def reviews
+    Review.where(game_id: self.id)
+  end
+
+  def create_game (title)
+    self.reviews << Review.new(score: score, comment: comment, game_id: self.id)
+  end
 end
